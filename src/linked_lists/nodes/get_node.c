8acaf2e1e5020f1d11002ecb40fe8abc5073c2aa
Luna <luna7111@tuta.io>
2024-12-26 01:09:23 +0100

    fixed some errors in node functions, started stack operation functions, stack_reverse_rotate is incomplete

--- src/linked_lists/nodes/get_node.c
+++ src/linked_lists/nodes/get_node.c
@@ -6,14 +6,14 @@
 /*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2024/12/25 22:40:35 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2024/12/25 23:07:13 by ldel-val          ``                     */
+/*   Updated: 2024/12/26 00:23:33 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
 t_stack	*last_node(t_stack *list)
 (
 	if (!list)
-		return (NULL;
+		return (NULL);
 	while (list->next)
 		list = list->next;
 	return (list);
---
+++ src/linked_lists/stack_operations/swap.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                    _.._  .           .     */
+/*   swap.c                                         .' .-'`        *          */
+/*                                                 /  /       +        *      */
+/*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
+/*                                                 \  '.___.;       +         */
+/*   Created: 2024/12/25 23:56:46 by ldel-val       '._  _.'   .        .     */
+/*   Updated: 2024/12/26 00:39:03 by ldel-val          ``                     */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <push_swap.h>
+
+void	stack_swap(t_stack **stack)
+{
+	int	buffer;
+
+	if (!stack || !*stack || !(*stack)->next)
+		return ;
+	buffer = *stack->nb;
+	*stack->nb = *stack->next->nb;
+	*stack->next->nb = buffer;
+}
+
+void	stack_push(t_stack **pusher, t_stack **pushee)
+{
+	node_add_front(pushee, *pusher);
+	remove_node(pusher);
+}
+
+void	stack_rotate(t_stack **stack)
+{
+	t_stack buffer;
+
+	if (!stack || !*stack || !*stack->next)
+		return ;
+	last_node(*stack)->next = *stack;
+	buffer = *stack;
+	*stack = stack->next;
+	buffer->next = NULL; 
+}
+
+void	stack_reverse_rotate(t_stack **stack)
+{
+	t_stack *buffer;
+
+	if (!stack || !*stack || !*stack->next)
+		return ;
+	buffer = *stack;
+	*stack = last_node(*stack);
+
+}
