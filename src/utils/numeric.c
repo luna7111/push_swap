f35b3e21444d8e9a38b62eee7b380f3d34a83280
Luna <luna7111@tuta.io>
2024-12-27 22:54:32 +0100

    added median of 3 function

---
+++ src/utils/numeric.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                    _.._  .           .     */
+/*   numeric.c                                      .' .-'`        *          */
+/*                                                 /  /       +        *      */
+/*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
+/*                                                 \  '.___.;       +         */
+/*   Created: 2024/12/27 11:06:48 by ldel-val       '._  _.'   .        .     */
+/*   Updated: 2024/12/27 18:07:47 by ldel-val          ``                     */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <push_swap.h>
+
+t_stack	median_of_three(t_stack *stack, t_stack *start, t_stack *end)
+{
+	t_stack	*middle;
+
+	middle = nth_node((node_position(end) - node_position(start)) / 2);
+	if (start->nb > middle->nb && start->nb < end->nb)
+		return (start);
+	if (start->nb < middle->nb && start->nb > end->nb)
+		return (start);
+	if (middle->nb > start->nb && middle->nb < end->nb)
+		return (middle);
+	if (middle->nb < start->nb && middle->nb > end->nb)
+		return (middle);
+	if (end->nb > start->nb && end->nb < middle->nb)
+		return (end);
+	if (end->nb < start->nb && end->nb > middle->nb)
+		return (end);
+	return (NULL);
+}
