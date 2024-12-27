869e32fd99e55cb603fd358c24d07b099b51f0bb
Luna <luna7111@tuta.io>
2024-12-27 22:55:13 +0100

    Added stack size and node position functions

---
+++ src/linked_lists/other.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                    _.._  .           .     */
+/*   other.c                                        .' .-'`        *          */
+/*                                                 /  /       +        *      */
+/*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
+/*                                                 \  '.___.;       +         */
+/*   Created: 2024/12/27 11:12:24 by ldel-val       '._  _.'   .        .     */
+/*   Updated: 2024/12/27 14:12:04 by ldel-val          ``                     */
+/*                                                                            */
+/* ************************************************************************** */
+
+size_t	stack_size(t_stack *stack)
+{
+	size_t	size;
+
+	size = 0;
+	while (stack != NULL)
+	{
+		size ++;
+		stack = stack->next;
+	}
+	return (size);
+}
+
+size_t	node_position(t_stack *stack, t_stack *node)
+{
+	size_t	position;
+
+	position = 0;
+	while (stack->next != NULL && stack != node)
+	{
+		position ++;
+		stack = stack->next;
+	}
+	return (position);
+}
