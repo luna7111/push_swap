7ac4e1510385c2389dabcad99550efdc3be14e28
Luna <luna7111@tuta.io>
2024-12-28 03:10:50 +0100

    updated makefile and header file

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: ldel-val <ldel-val@42madrid.com>          |  |           *            #
 #                                                  \  '.___.;       +          #
 #    Created: 2024/11/25 12:45:41 by ldel-val       '._  _.'   .        .      #
-#    Updated: 2024/12/26 22:01:40 by ldel-val          ``                      #
+#    Updated: 2024/12/28 02:13:54 by ldel-val          ``                      #
 #                                                                              #
 # **************************************************************************** #
 
@@ -25,11 +25,19 @@ LIB 	:=	$(LIBFT)
 
 NAME 	:=	push_swap
 SRC 	:=	src/push_swap.c\
+			src/sort.c\
+			src/check.c\
 			src/linked_lists/add_remove.c\
 			src/linked_lists/create_delete.c\
 			src/linked_lists/get_node.c\
 			src/linked_lists/stack_operations.c\
-			src/utils/format.c
+			src/linked_lists/other.c\
+			src/utils/format.c\
+			src/utils/numeric.c\
+			src/operations/push.c\
+			src/operations/swap.c\
+			src/operations/rotate.c\
+			src/operations/reverse_rotate.c
 OBJ		:=	$(SRC:.c=.o)
 
 #-----------------------------------------------------------------------------#
--- src/push_swap.h
+++ src/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2024/12/25 21:32:16 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2024/12/27 12:01:23 by ldel-val          ``                     */
+/*   Updated: 2024/12/28 02:11:22 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ typedef struct s_stack
 	struct	s_stack *next;
 }	t_stack;
 
+void	error(int n);
+
 void	node_add_front(t_stack **list, t_stack *node);
 void	node_add_back(t_stack **list, t_stack *node);
 void	remove_node(t_stack **node);
@@ -29,10 +31,11 @@ void	remove_node(t_stack **node);
 t_stack	*new_node(int nb);
 void	delete_node(t_stack *node);
 
-t_stack *last_node(t_stack *list);
+t_stack *last_node(t_stack *stack);
 t_stack *nth_node(t_stack *node, int index);
 t_stack	*next_to_last_node(t_stack *node);
 size_t	stack_size(t_stack *stack);
+size_t	node_position(t_stack *stack, t_stack *node);
 
 void	stack_swap(t_stack **stack);
 void	stack_push(t_stack **pusher, t_stack **pushee);
@@ -41,5 +44,25 @@ void	stack_reverse_rotate(t_stack **stack);
 
 int	str_is_int(char *str);
 
-int	median_of_three(t_stack *stack);
+t_stack	*median_of_three(t_stack *start, t_stack *end);
+
+int		section_is_smaller(t_stack *start, t_stack *end, int nb);
+int		section_is_bigger(t_stack *start, t_stack *end, int nb);
+int		is_ordered(t_stack *stack);
+void	check_stack(t_stack *stack);
+
+void	o_sa(t_stack **stack);
+void	o_sb(t_stack **stack);
+void	o_ss(t_stack **stack_a, t_stack **stack_b);
+void	o_pa(t_stack **stack_a, t_stack **stack_b);
+void	o_pb(t_stack **stack_a, t_stack **stack_b);
+void	o_ra(t_stack **stack);
+void	o_rb(t_stack **stack);
+void	o_rr(t_stack **stack_a, t_stack **stack_b);
+void	o_rra(t_stack **stack);
+void	o_rrb(t_stack **stack);
+void	o_rrr(t_stack **stack_a, t_stack **stack_b);
+
+void	sort(t_stack **sa, t_stack **sb);
+
 #endif
