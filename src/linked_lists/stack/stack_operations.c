42af03a79744698c7a8e8f4ebf75cdf35f044207
Luna del Valle <luna7111@tuta.io>
2025-02-11 20:09:13 +0100

    Added "stack_push" function

--- src/linked_lists/stack/stack_operations.c
+++ src/linked_lists/stack/stack_operations.c
@@ -6,7 +6,7 @@
 /*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2025/02/11 19:59:08 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2025/02/11 20:08:14 by ldel-val          ``                     */
+/*   Updated: 2025/02/11 20:09:04 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,13 @@ void	stack_swap(t_stack **stack)
 	buffer = (*stack)->nb;
 	(*stack)->nb = (*stack)->next->nb;
 	(*stack)->next->nb = buffer;
+}
+
+void	stack_push(t_stack **pusher, t_stack **pushee)
+{
+	t_stack	*buffer;
+
+	buffer = *pusher;
+	*pusher = (*pusher)->next;
+	node_add_front(pushee, buffer);
 }
