7e282bdaf4df836bb4152b01c754579e2b82ecbe
Luna del Valle <luna7111@tuta.io>
2025-02-11 21:33:14 +0100

    Added oplst clear and delete functions

---
+++ src/linked_lists/oplst/clear.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                    _.._  .           .     */
+/*   clear.c                                        .' .-'`        *          */
+/*                                                 /  /       +        *      */
+/*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
+/*                                                 \  '.___.;       +         */
+/*   Created: 2025/02/11 21:11:03 by ldel-val       '._  _.'   .        .     */
+/*   Updated: 2025/02/11 21:32:48 by ldel-val          ``                     */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <push_swap.h>
+
+void	del_op(t_oplst **operation)
+{
+	t_oplst *next;
+
+	next = operation->next;
+	free(operation);
+	operation = next;
+}
+
+void	clear_oplst(t_oplst **list)
+{
+	while (*list != NULL)
+		del_op(*list);
+}
