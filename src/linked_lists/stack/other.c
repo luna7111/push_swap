239263183e984749094c250d6933d42d9c69f465
Luna del Valle <luna7111@tuta.io>
2025-02-11 20:34:54 +0100

    Added "stack_size" and "node_position"

---
+++ src/linked_lists/stack/other.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                    _.._  .           .     */
+/*   other.c                                        .' .-'`        *          */
+/*                                                 /  /       +        *      */
+/*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
+/*                                                 \  '.___.;       +         */
+/*   Created: 2025/02/11 20:13:53 by ldel-val       '._  _.'   .        .     */
+/*   Updated: 2025/02/11 20:31:15 by ldel-val          ``                     */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <push_swap.h>
+
+long	stack_size(t_stack *stack)
+{
+	long	size;
+
+	size = 0;
+	while (stack != NULL)
+	{
+		size ++;
+		stack = stack->next;
+	}
+	return (size);
+}
+
+long	node_position(t_stack *stack, t_stack *node)
+{
+	long	position;
+
+	position = 0;
+	while (stack->next != NULL && stack != node)
+	{
+		position ++;
+		stack = stack->next;
+	}
+}
