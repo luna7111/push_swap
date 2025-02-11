04d36106e852abf2328aad9d39883a371e3ecac0
Luna del Valle <luna7111@tuta.io>
2025-02-11 20:08:28 +0100

    added "stack_swap" function

--- src/linked_lists/stack/stack_operations.c
+++ src/linked_lists/stack/stack_operations.c
@@ -6,8 +6,22 @@
 /*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2025/02/11 19:59:08 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2025/02/11 19:59:39 by ldel-val          ``                     */
+/*   Updated: 2025/02/11 20:08:14 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <push_swap.h>
+
+void	stack_swap(t_stack **stack)
+{
+	int	buffer;
+
+	if (!stack || !*stack || !(*stack)->next)
+		return ;
+	buffer = (*stack)->index;
+	(*stack)->index = (*stack)->next->index;
+	(*stack)->next->index = buffer;
+	buffer = (*stack)->nb;
+	(*stack)->nb = (*stack)->next->nb;
+	(*stack)->next->nb = buffer;
+}
