6487fd1a5c7cdd8df6daa8ef2f6655424503fb1b
Luna del Valle <luna7111@tuta.io>
2025-02-11 18:52:36 +0100

    Added header file with define protection, makros for push_swap operations and libft

---
+++ src/push_swap.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                    _.._  .           .     */
+/*   push_swap.h                                    .' .-'`        *          */
+/*                                                 /  /       +        *      */
+/*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
+/*                                                 \  '.___.;       +         */
+/*   Created: 2025/02/11 18:48:42 by ldel-val       '._  _.'   .        .     */
+/*   Updated: 2025/02/11 18:51:32 by ldel-val          ``                     */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "../libft/libft.h"
+
+# define SA 1
+# define SB 2
+# define SS 3
+# define PA 4
+# define PB 5
+# define RA 6
+# define RB 7
+# define RR 8
+# define RRA 9
+# define RRB 10
+# define RRR 11
+
+#endif
