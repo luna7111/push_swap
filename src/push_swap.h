f9c71d8e53f860510ddafd918390c3f22d300238
Luna del Valle <luna7111@tuta.io>
2025-02-12 00:39:12 +0100

    Added operation prototypes

--- src/push_swap.h
+++ src/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2025/02/11 18:48:42 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2025/02/11 22:34:59 by ldel-val          ``                     */
+/*   Updated: 2025/02/12 00:38:32 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,5 +68,21 @@ void	del_op(t_oplst **operation);
 void	clear_oplst(t_oplst **list);
 // |_print.c
 void	print_list(t_oplst *list);
+
+//operations
+void	o_pa(t_stack **stack_a, t_stack **stack_b, t_oplst **oplst);
+void	o_pb(t_stack **stack_a, t_stack **stack_b, t_oplst **oplst);
+void	o_rra(t_stack **stack, t_oplst **oplst);
+void	o_rrb(t_stack **stack, t_oplst **oplst);
+void	o_rrr(t_stack **stack_a, t_stack **stack_b, t_oplst **oplst);
+void	o_ra(t_stack **stack, t_oplst **oplst);
+void	o_rb(t_stack **stack, t_oplst **oplst);
+void	o_rr(t_stack **stack_a, t_stack **stack_b, t_oplst **oplst);
+void	o_ra(t_stack **stack, t_oplst **oplst);
+void	o_rb(t_stack **stack, t_oplst **oplst);
+void	o_rr(t_stack **stack_a, t_stack **stack_b, t_oplst **oplst);
+void	o_sa(t_stack **stack, t_oplst **oplst);
+void	o_sb(t_stack **stack, t_oplst **oplst);
+void	o_ss(t_stack **stack_a, t_stack **stack_b, t_oplst **oplst);
 
 #endif
