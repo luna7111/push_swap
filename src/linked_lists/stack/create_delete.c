dbe7c555e56c9f923aed7d21cd32b1454083310c
Luna del Valle <luna7111@tuta.io>
2025-02-11 19:57:36 +0100

    Added "new_node" and "delete_node" functions

---
+++ src/linked_lists/stack/create_delete.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                    _.._  .           .     */
+/*   create_delete.c                                .' .-'`        *          */
+/*                                                 /  /       +        *      */
+/*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
+/*                                                 \  '.___.;       +         */
+/*   Created: 2025/02/11 19:51:53 by ldel-val       '._  _.'   .        .     */
+/*   Updated: 2025/02/11 19:56:30 by ldel-val          ``                     */
+/*                                                                            */
+/* ************************************************************************** */
+
+t_stack	*new_node(int nb)
+{
+	t_stack	*node;
+
+	node = (t_stack *)malloc(sizeof(t_stack));
+	if (!node)
+		return (NULL);
+	node->nb = nb;
+	node->index = -1;
+	node->block = 0;
+	node->next = NULL;
+	return (node);
+}
+
+void	delete_node(t_stack **node)
+{
+	t_stack *next;
+
+	next = (*node)->next;
+	free(*node);
+	*node = next;
+}
