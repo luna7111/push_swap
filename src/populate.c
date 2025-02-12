22adda5205c3d6974100ceff75df310df2b66c3d
Luna del Valle <luna7111@tuta.io>
2025-02-12 12:21:10 +0100

    Added error protections to avoid leaks

--- src/populate.c
+++ src/populate.c
@@ -6,7 +6,7 @@
 /*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2025/02/12 10:17:21 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2025/02/12 11:25:14 by ldel-val          ``                     */
+/*   Updated: 2025/02/12 12:17:58 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,21 @@
 
 int	argument_is_valid(char *arg, t_stack *stack)
 {
-	if (ft_strncmp(ft_itoa(ft_atoi(arg)), arg, ft_strlen(arg)))
+	char	*ideal;
+
+	ideal = ft_itoa(ft_atoi(arg));
+	if (ft_strncmp(ideal, arg, ft_strlen(arg)))
+	{
+		free(ideal);
 		return (0);
+	}
 	while (stack != NULL)
 	{
 		if (ft_atoi(arg) == stack->nb)
 			return (0);
 		stack = stack->next;
 	}
+	free(ideal);
 	return (1);
 }
 
@@ -29,7 +36,7 @@ int	stack_is_ordered(t_stack *stack)
 {
 	if (stack == NULL)
 		return (-1);
-	while (stack->next != NULL)
+	while (stack != NULL && stack->next != NULL)
 	{
 		if (stack->nb > stack->next->nb)
 			return (0);
@@ -38,27 +45,31 @@ int	stack_is_ordered(t_stack *stack)
 	return (1);
 }
 
-t_stack *populate_stack(int argn, char **argv)
+t_stack	*populate_stack(int argn, char **argv)
 {
-	t_stack *head;
+	t_stack	*head;
 	char	**split_argument;
 	int		i;
 
-	i = 0;
+	head = NULL;
 	while (--argn)
 	{
+		i = 0;
 		split_argument = ft_split(argv[argn], ' ');
-		while (split_argument[i])
+		while (split_argument[i] != NULL)
 			i++;
 		while (i--)
 		{
 			if (!argument_is_valid(split_argument[i], head))
-				error();
+				error(&head, split_argument);
 			stack_add_front(&head, new_node(ft_atoi(split_argument[i])));
 		}
 		free_strarray(split_argument);
 	}
 	if (stack_is_ordered(head))
+	{
+		clear_stack(&head);
 		exit(0);
+	}
 	return (head);
 }
