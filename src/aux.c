a0320dfcf55c10f5251e2ece05385c62ab77cce6
Luna del Valle <luna7111@tuta.io>
2025-02-12 23:27:15 +0100

    Added block_average function

--- src/aux.c
+++ src/aux.c
@@ -6,7 +6,7 @@
 /*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2025/02/12 10:05:38 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2025/02/12 13:09:31 by ldel-val          ``                     */
+/*   Updated: 2025/02/12 17:26:17 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,4 +57,25 @@ void	indexize_stack(t_stack *stack)
 		last_node = next_node;
 		index ++;
 	}
+}
+
+double	block_average(t_stack *stack)
+{
+	int		block;
+	double	block_size;
+	double	average;
+
+	block_size = 0;
+	average = 0;
+	block = stack->block;
+	while (stack != NULL)
+	{
+		if (stack->block == block)
+		{
+			block_size ++;
+			average += stack->index;
+		}
+		stack = stack->next;
+	}
+	return ((float)average / block_size);
 }
