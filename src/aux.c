7dcb3874d9dd7efb75c4bdf761f2531066a6e76e
Luna del Valle <luna7111@tuta.io>
2025-02-12 10:51:19 +0100

    added stack_is_sorted function

--- src/aux.c
+++ src/aux.c
@@ -6,7 +6,7 @@
 /*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2025/02/12 10:05:38 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2025/02/12 10:14:00 by ldel-val          ``                     */
+/*   Updated: 2025/02/12 10:42:00 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,17 @@ void	error(void)
 {
 	write(STDERR, "Error\n", 6);
 	exit(0);
+}
+
+int	stack_is_sorted(t_stack *stack)
+{
+	if (stack == NULL)
+		return (-1);
+	while (stack->next != NULL)
+	{
+		if (stack->index + 1 != stack->next->index)
+			return (0);
+		stack = stack->next;
+	}
+	return (1);
 }
