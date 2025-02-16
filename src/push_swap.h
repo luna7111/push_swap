46a6400167e1ee1052751c3a3ee41c4897c8d5ab
Luna del Valle <luna7111@tuta.io>
2025-02-16 16:50:36 +0100

    included oplist function prototypes

--- src/push_swap.h
+++ src/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2025/02/16 16:06:16 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2025/02/16 16:10:13 by ldel-val          ``                     */
+/*   Updated: 2025/02/16 16:47:58 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,12 @@ typedef struct	s_oplist
 {
 	int				op;
 	struct s_oplist *nx;
-}
+}	t_oplst;
+
+//oplists
+
+t_oplst *add_op(t_oplst **list, int op_nb);
+void	clear_oplst(t_oplst **list);
+void	print_list(t_oplst *list);
 
 #endif
